fdda99a5c14efdd92abf91e4214ef90688a7bce7
Janka <jankamamatejova@gmail.com>
2025-12-05 13:42:01 +0100

    added logos, changed navbar

--- App/Views/Layouts/root.layout.view.php
+++ App/Views/Layouts/root.layout.view.php
@@ -25,23 +25,48 @@
 <body>
 <nav class="navbar navbar-expand-sm navbar-custom">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?= $link->url('home.index') ?>">
+        <a class="navbar-brand d-flex align-items-center" href="<?= $link->url('home.index') ?>">
             <img src="<?= $link->asset('images/cat_logo.png') ?>" title="<?= App\Configuration::APP_NAME ?>" alt="CatTracker Logo">
+            <span class="site-name ms-2"><?= App\Configuration::APP_NAME ?></span>
         </a>
-        <?php if ($user->isLoggedIn()) { ?>
-            <span class="navbar-text">Logged in user: <b><?= $user->getName() ?></b></span>
-            <ul class="navbar-nav ms-auto">
+
+        <!-- Right-side controls: Cat Database, Map View, and auth actions -->
+        <ul class="navbar-nav ms-auto align-items-center">
+            <li class="nav-item me-2">
+                <a class="nav-link btn btn-outline-dark d-flex align-items-center" href="<?= $link->url('cats.index') ?>">
+                    <!-- simple cat icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-2" viewBox="0 0 16 16" aria-hidden="true">
+                        <path d="M2.165 4.5c-.5.2-.99.7-1.01 1.3C1.08 8.7 3.6 11 8 11s6.92-2.3 6.845-5.2c-.02-.6-.51-1.1-1.01-1.3-.08-.03-.16.04-.19.12C12.9 6 11.8 6.5 10 6.5s-2.9-.5-3.8-1.08c-.11-.07-.26-.07-.37 0C5.1 6 4 6.5 2.205 5.62c-.03-.08-.11-.15-.04-.12z"/>
+                    </svg>
+                    Cat Database
+                </a>
+            </li>
+            <li class="nav-item me-2">
+                <a class="nav-link btn btn-outline-dark d-flex align-items-center" href="<?= $link->url('map.index') ?>">
+                    <!-- simple map marker icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="me-2" viewBox="0 0 16 16" aria-hidden="true">
+                        <path d="M8 0a5 5 0 0 0-5 5c0 4.667 5 11 5 11s5-6.333 5-11a5 5 0 0 0-5-5zm0 8a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                    </svg>
+                    Map View
+                </a>
+            </li>
+
+            <?php if ($user && $user->isLoggedIn()) { ?>
+                <li class="nav-item me-2">
+                    <span class="navbar-text">Logged in: <strong><?= htmlspecialchars($user->getName()) ?></strong></span>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?= $link->url('auth.logout') ?>">Log out</a>
                 </li>
-            </ul>
-        <?php } else { ?>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= App\Configuration::LOGIN_URL ?>">Log in</a>
+            <?php } else { ?>
+                <li class="nav-item me-2">
+                    <a class="nav-link btn btn-outline-dark " href="<?= App\Configuration::LOGIN_URL ?>">Log in</a>
                 </li>
-            </ul>
-        <?php } ?>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-dark" href="<?= $link->url('auth.signup') ?>">Sign up</a>
+                </li>
+            <?php } ?>
+        </ul>
     </div>
 </nav>
 <div class="container-fluid mt-3">
